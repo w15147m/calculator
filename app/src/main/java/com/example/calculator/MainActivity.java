1ef5cb816ca8e711a89555696558872712c678d2
waseem abbas <w15147m@gmail.com>
2023-06-30 01:52:23 -0700

    calculator 1.0

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+  TextView resultTv,solutionTv;
+   MaterialButton buttonc,buttonfb,buttonbb,buttonAC,buttondot;
+   MaterialButton buttondivide,buttonmultiply,buttonminus,buttonplus,buttonequls;
+   MaterialButton button1,button2,button3,button4,button5,button6,button7,button8,button9,button0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+  resultTv = findViewById(R.id.result_tv);
+  solutionTv = findViewById(R.id.solution_tv);
+        solutionTv.setText("");
+        assingId(R.id.button_c);
+        assingId(R.id.button_bb);
+        assingId(R.id.button_fb);
+        assingId(R.id.button_divide);
+        assingId(R.id.button_1);
+        assingId(R.id.button_2);
+        assingId(R.id.button_3);
+        assingId(R.id.button_multiply);
+        assingId(R.id.button_4);
+        assingId(R.id.button_5);
+        assingId(R.id.button_6);
+        assingId(R.id.button_plus);
+        assingId(R.id.button_7);
+        assingId(R.id.button_8);
+        assingId(R.id.button_9);
+        assingId(R.id.button_minus);
+        assingId(R.id.button_AC);
+        assingId(R.id.button_0);
+        assingId(R.id.button_dot);
+        assingId(R.id.button_equals);
+
+
+
+
+    }
+ void assingId(int id){
+
+     MaterialButton btn = findViewById(id);
+        btn.setOnClickListener(this);
+
+
+
+ }
+
+    @Override
+    public void onClick(View v) {
+
+
+        MaterialButton button = (MaterialButton) v;
+        String btnText  = button.getText().toString();
+        String dataToCalculat = solutionTv.getText().toString();
+
+
+
+
+        if (btnText.equals("AC")){
+
+            solutionTv.setText("");
+            resultTv.setText("0");
+
+
+        }
+
+      if (btnText.equals("=")){
+
+            resultTv.setText(solutionTv.getText());
+
+        }
+
+
+     if(btnText.equals("c")){
+
+            dataToCalculat = dataToCalculat.substring(0,dataToCalculat.length()-1);
+
+        }else  {
+
+            dataToCalculat = dataToCalculat + btnText;
+
+        }
+
+
+
+
+        solutionTv.setText(dataToCalculat);
+
+    }
+}
